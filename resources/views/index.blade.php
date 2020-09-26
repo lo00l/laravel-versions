fda2a610b3c88b3af9e763b455a11cb685615821
Aleksey <aleksey.chita@gmail.com>
2020-09-26 12:11:58 +0300

    update resources

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>How Many Times Has Laravel Been Released This Month?</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <div class="container count">
+            <h1>{{ $releases->count() }}</h1>
+            <a href="#releases" class="info">What versions?</a>
+        </div>
+        <a name="releases"></a>
+        <div class="container releases">
+            <ul>
+                @foreach($releases as $release)
+                    <?php /** @var \App\Models\Release $release */ ?>
+                    <li>
+                        <a href="{{ $release->url }}">
+                            {{ $release->tag_name }} | {{ $release->released_at->format('m/d/Y') }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+
+        </div>
+
+        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
